a927c09f9a07f5de12ed62d63469496b89dccef8
Anas <anasarifin1995@gmail.com>
2022-09-26 11:31:46 +0700

    add places service

--- src/index.ts
+++ src/index.ts
@@ -34,17 +34,15 @@ import { Point } from "./maps/coordinates/point";
 import { Polygon } from "./drawing/polygons/polygon";
 import { Polyline } from "./drawing/polygons/polyline";
 import { SearchBox } from "./places/searchbox";
+import { AutocompleteService } from "./places/autocompleteservice";
+import { PlacesService } from "./places/placesservice";
 import { Size } from "./maps/coordinates/size";
 import { VisibleRegion } from "./maps/maps/visibleregion";
 import { DirectionsService } from "./drawing/directions/service";
 import { DirectionsRenderer } from "./drawing/directions/renderer";
 import { StreetViewCoverageLayer } from "./street-view/rendering/coverage";
 import { StreetViewPanorama } from "./street-view/rendering/panorama";
-import {
-  StreetViewPreference,
-  StreetViewSource,
-  StreetViewStatus,
-} from "./street-view/service/constants";
+import { StreetViewPreference, StreetViewSource, StreetViewStatus } from "./street-view/service/constants";
 import { StreetViewService } from "./street-view/service/service";
 import { ControlPosition } from "./maps/controls/controlposition";
 import { MapTypeId } from "./maps/maps/constants";
@@ -54,77 +52,85 @@ import { spherical } from "./maps/geometry/spherical";
 import { mockInstances } from "./registry";
 
 const initialize = function (): void {
-  mockInstances.clearAll();
-  (global as any).google = {
-    maps: {
-      ImageMapType: jest.fn(),
-      Marker: Marker,
-      Map: Map_,
-      Data: Data,
-      Point: Point,
-      Size: Size,
-      MVCObject: MVCObject,
-      MVCArray: MVCArray,
-      MapTypeId: MapTypeId,
-      ControlPosition: ControlPosition,
-      LatLng: LatLng,
-      LatLngBounds: LatLngBounds,
-      StreetViewPanorama: StreetViewPanorama,
-      StreetViewCoverageLayer: StreetViewCoverageLayer,
-      StreetViewService: StreetViewService,
-      StreetViewPreference: StreetViewPreference,
-      StreetViewSource: StreetViewSource,
-      StreetViewStatus: StreetViewStatus,
-      event: event,
-      places: {
-        Autocomplete: Autocomplete,
-        SearchBox: SearchBox,
-      },
-      geometry: {
-        spherical:  spherical
-      },
-      Polygon: Polygon,
-      Polyline: Polyline,
-      Circle: Circle,
-      OverlayView: OverlayView,
-      KmlLayer: KmlLayer,
-      MapCanvasProjection: MapCanvasProjection,
-      MapPanes: MapPanes,
-      VisibleRegion: VisibleRegion,
-      DirectionsService: DirectionsService,
-      DirectionsRenderer: DirectionsRenderer,
-      InfoWindow: InfoWindow_,
-    },
-  };
+    mockInstances.clearAll();
+    (global as any).google = {
+        maps: {
+            ImageMapType: jest.fn(),
+            Marker: Marker,
+            Map: Map_,
+            Data: Data,
+            Point: Point,
+            Size: Size,
+            MVCObject: MVCObject,
+            MVCArray: MVCArray,
+            MapTypeId: MapTypeId,
+            ControlPosition: ControlPosition,
+            LatLng: LatLng,
+            LatLngBounds: LatLngBounds,
+            StreetViewPanorama: StreetViewPanorama,
+            StreetViewCoverageLayer: StreetViewCoverageLayer,
+            StreetViewService: StreetViewService,
+            StreetViewPreference: StreetViewPreference,
+            StreetViewSource: StreetViewSource,
+            StreetViewStatus: StreetViewStatus,
+            event: event,
+            places: {
+                Autocomplete: Autocomplete,
+                SearchBox: SearchBox,
+                AutocompleteService: AutocompleteService,
+                PlacesService: PlacesService,
+            },
+            geometry: {
+                spherical: spherical,
+            },
+            Polygon: Polygon,
+            Polyline: Polyline,
+            Circle: Circle,
+            OverlayView: OverlayView,
+            KmlLayer: KmlLayer,
+            MapCanvasProjection: MapCanvasProjection,
+            MapPanes: MapPanes,
+            VisibleRegion: VisibleRegion,
+            DirectionsService: DirectionsService,
+            DirectionsRenderer: DirectionsRenderer,
+            InfoWindow: InfoWindow_,
+        },
+    };
+};
+
+const places = {
+    AutocompleteService: AutocompleteService,
+    PlacesService: PlacesService,
 };
 
 export {
-  Circle,
-  Data,
-  event,
-  spherical,
-  Feature,
-  LatLng,
-  LatLngBounds,
-  MVCArray,
-  MVCObject,
-  MapCanvasProjection,
-  MapPanes,
-  Map_ as Map,
-  Marker,
-  OverlayView,
-  KmlLayer,
-  Point,
-  Polygon,
-  Polyline,
-  Size,
-  StreetViewCoverageLayer,
-  StreetViewPanorama,
-  StreetViewService,
-  VisibleRegion,
-  DirectionsService,
-  DirectionsRenderer,
-  InfoWindow_ as InfoWindow,
-  mockInstances,
-  initialize,
+    Circle,
+    Data,
+    event,
+    spherical,
+    Feature,
+    LatLng,
+    LatLngBounds,
+    MVCArray,
+    MVCObject,
+    MapCanvasProjection,
+    MapPanes,
+    Map_ as Map,
+    Marker,
+    OverlayView,
+    KmlLayer,
+    Point,
+    Polygon,
+    Polyline,
+    Size,
+    StreetViewCoverageLayer,
+    StreetViewPanorama,
+    StreetViewService,
+    VisibleRegion,
+    DirectionsService,
+    DirectionsRenderer,
+    InfoWindow_ as InfoWindow,
+    places,
+    mockInstances,
+    initialize,
 };
